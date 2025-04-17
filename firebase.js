10eccabd47c8b35abd20396fb907305ec0adc0b0
Pragya Singh <113745975+pragya-singh-17@users.noreply.github.com>
2025-04-17 20:17:14 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 //Replaced
 const firebaseConfig = {
-    apiKey: "AIzaSyAsf0Nyl5GFxfp9rmEtbm57YxGLp0Hm5tE",
-    authDomain: "annasetu-4d91e.firebaseapp.com",
-    projectId: "annasetu-4d91e",
-    storageBucket: "annasetu-4d91e.firebasestorage.app",
-    messagingSenderId: "897036862185",
-    appId: "1:897036862185:web:e33817f28bf3bdbabc6678"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
